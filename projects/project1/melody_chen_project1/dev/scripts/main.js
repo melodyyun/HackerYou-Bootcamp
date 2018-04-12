a4498ea8745f07a00ba020d13394fca07d67a782
melodyyun <melodyyunc@gmail.com>
2018-04-11 23:38:37 -0400

    project 1 navbar update

--- projects/project1/melody_chen_project1/dev/scripts/main.js
+++ projects/project1/melody_chen_project1/dev/scripts/main.js
@@ -1,1 +1,17 @@
-console.log("ready")
+const app ={};
+
+app.nav = () => {
+    const navHome = $("#home");
+    navHome.on("click", function(){
+        console.log("clicked on nav bar");
+    });
+};
+
+app.init = () => {
+    app.nav();
+};
+
+// document ready;
+$(document).ready(function(){
+    app.init();
+});
--- projects/project1/melody_chen_project1/public/scripts/main.js
+++ projects/project1/melody_chen_project1/public/scripts/main.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("ready");
+var app = {};
+
+app.nav = function () {
+    var navHome = $("#home");
+    navHome.on("click", function () {
+        console.log("clicked on nav bar");
+    });
+};
+
+app.init = function () {
+    app.nav();
+};
+
+// document ready;
+$(document).ready(function () {
+    app.init();
+});

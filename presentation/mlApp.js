f1d0b71833751ab6e58fdf5c0a8672887f16d314
Melody Yun Chen <Melodyyunc@gmail.com>
2018-05-03 10:36:06 -0400

    javascript organization

--- presentation/mlApp.js
+++ presentation/mlApp.js
@@ -21,7 +21,7 @@ input.addEventListener("change", (e) => {
         //green
         { input: { r: 0, g: 1, b: 0 }, output: { light: 1 } },
         //blue
-        { input: { r: 0, g: 0, b: 1 }, output: { light: 1 } },
+        { input: { r: 0, g: 0, b: 1 }, output: { dark: 1 } },
         
         //black
         { input: { r: 0, g: 0, b: 0}, output: { dark: 1 } },
